470a25cf5eeb3f3c717dfe19e1e9a3d8613f5e9b
fairuzathrz <fayizpdp2008@gmai.com>
2026-06-01 10:23:40 +0700

    menutuo file witer dan menyiapkan file reader

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -1,14 +1,10 @@
 
-    //unlimited loop untuk menulis file
-    while (true)
-    {
-        
-        cout << "- ";
-        //mendapat setiap karakter dalam satu baris
-        getline(cin, baris);
-        //kondisi false jika baris hanya berisi karakter q
-        if (baris == "q") break;
-        //menulis dan memasukkan nilai dari variabel baris ke dalam file
-        outfile << baris << endl;
-    }
-   
+    //menutup file setelah penulisan selesai
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    
