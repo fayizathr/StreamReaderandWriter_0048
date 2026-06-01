310e37571169ac3a1ca15644a625ffbb9d7c8055
fairuzathrz <fayizpdp2008@gmai.com>
2026-06-01 10:33:03 +0700

    membuka ifstream untuk membaca file dinamis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -1,19 +1,9 @@
 
 
-    //looping untuk menulis isi file
-    while (true)
-    {
-
-        cout << "- ";
-        //mendapatkan setiap karakter dalam satu baris
-        getline(cin, baris);
-        //kondisi jika baris hanya berisi huruf q
-        if (baris == "q") break;
-        //menulis dan memasukkan nilai dari baris ke dalam file
-        outfile << baris << endl;
-
-    }
-    //menutup file apabila telah selesai di tulis
-    outfile.close();
+    //membuka file dalam mode membaca
+    ifstream infile;
+    
+    //menunjuk ke sebuah file dengan ekstensi .txt
+    infile.open(NamaFile + ".txt", ios::in);
 
     
